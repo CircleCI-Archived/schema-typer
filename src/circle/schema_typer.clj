37475efd6a64ffb84e42c754bc4adbc82d93db7f
Allen Rohner <arohner@gmail.com>
2013-11-02 16:08:53 -0700

    Typing

--- src/circle/schema_typer.clj
+++ src/circle/schema_typer.clj
@@ -1,17 +1,25 @@
 (ns circle.schema-typer
-  "Creates core.typed types from prismatic/schema definitions. Inspired by https://gist.github.com/c-spencer/6569571"
-  (:import (clojure.lang Keyword))
-  (:require [clojure.core.typed :as t :refer (ann def-alias)]))
+  "Creates core.typed types from prismatic/schema
+   definitions. Inspired by https://gist.github.com/c-spencer/6569571"
+  (:import (clojure.lang Symbol Keyword IMapEntry))
+  (:require [clojure.core.typed :as t :refer (ann def-alias)]
+            [schema.core :as s]))
+
+(t/warn-on-unannotated-vars)
+
+(t/ann clojure.core/val (All [x] (Fn [(clojure.lang.IMapEntry Any x) -> x])))
 
 ;; refine these later
-(def-alias Schema Any)
+(def-alias Schema (U Number (t/Map Any Any) (HMap)))
 (def-alias CoreType (U Symbol (t/Seq Any)))
 
+(t/ann convert-dispath [Schema -> Class])
 (defn convert-dispatch [schema]
   (cond
    (class? schema) schema
    :else (class schema)))
 
+(t/ann convert [Schema -> CoreType])
 (defmulti convert "" #'convert-dispatch)
 
 (defmethod convert Number [s]
@@ -26,14 +34,15 @@
 (defmethod convert clojure.lang.Symbol [s]
   s)
 
-(t/ann hmap-grouper [Any -> (U (Value :mandatory)
-                               (Value :optional))])
+(t/ann hmap-grouper [IMapEntry -> (U (Value :mandatory)
+                                     (Value :optional))])
 (defn hmap-grouper
   [kv]
   (if (= schema.core.OptionalKey (class (key kv)))
     :optional
     :mandatory))
 
+(t/ann convert-hmap [(HMap) -> CoreType])
 (defn convert-hmap
   "Returns a core.typed HMap."
   [s]
@@ -52,23 +61,27 @@
           :mandatory (convert-kvs mandatory)
           :optional (convert-kvs optional))))
 
-(defn class->name [c]
+(t/ann class->name [Class -> Symbol])
+(defn class->name [^Class c]
   (-> c .getName symbol))
 
+(t/ann convert-map [(t/Map Any Any) -> CoreType])
 (defn convert-map [s]
   (assert (map? s))
   (assert (= 1 (count s)) "convert-map only supports one kv")
   (let [kt (-> s first key)
         vt (-> s first val)]
-    (assert (class kt))
-    (assert (class vt))
+    (assert (= Class (class kt)))
+    (assert (= Class (class vt)))
     (list 't/Map (class->name kt) (class->name vt))))
 
+(t/ann non-hmap? [Schema -> Boolean])
 (defn non-hmap? [s]
   (and (map? s)
        (class? (first (keys s)))))
 
 (defmethod convert clojure.lang.IPersistentMap [s]
+  (assert (map? s))
   (if (non-hmap? s)
     (convert-map s)
     (convert-hmap s)))
--- test/circle/schema_type_def.clj
+++ test/circle/schema_type_def.clj
@@ -1,6 +1,11 @@
 (ns circle.schema-type-def
-  (:require [circle.schema-typer :as st]))
+  (:require [clojure.core.typed :as t]
+            [circle.schema-typer :as st]))
 
 ;; data used for testing
 (st/def-schema-type User {:login String
                           :login-count Number})
+
+(t/ann test-user User)
+(def test-user {:login "foo"
+                 :login-count 5})
