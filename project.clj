eb86077fc678bba382259ffa13eb2c8a3d333eeb
Allen Rohner <arohner@gmail.com>
2013-11-02 16:08:57 -0700

    Fix the project

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject schema-typer "0.1.0"
+(defproject circleci/schema-typer "0.1.0"
   :dependencies [[org.clojure/core.typed "0.2.14"]]
-  :profiles {:dev {:dependencies [org.clojure/clojure "1.5.1"]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
