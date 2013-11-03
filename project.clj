68a14bc10bd78d08c07b9ddc176426a13058bfa4
Allen Rohner <arohner@gmail.com>
2013-11-02 22:25:13 -0700

    schema is a dependency!

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject circleci/schema-typer "0.1.0"
-  :dependencies [[org.clojure/core.typed "0.2.14"]]
+  :dependencies [[org.clojure/core.typed "0.2.14"]
+                 [prismatic/schema "0.1.6"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
