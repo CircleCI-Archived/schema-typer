a608ed6d0186c88ec983b7c9315d2e3477123620
Allen Rohner <arohner@gmail.com>
2014-04-22 14:37:37 -0700

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject circleci/schema-typer "0.2.0"
-  :dependencies [[org.clojure/core.typed "0.2.31"]
+(defproject circleci/schema-typer "0.2.1"
                  [prismatic/schema "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
