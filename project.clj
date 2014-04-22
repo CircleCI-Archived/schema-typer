7d202f645fb14431fff24a13d7175cae5cc95d0c
Allen Rohner <arohner@gmail.com>
2014-04-22 14:40:57 -0700

    Fix the stupids

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject circleci/schema-typer "0.2.1"
-                 [prismatic/schema "0.2.1"]]
+  :dependencies [[prismatic/schema "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
