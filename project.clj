da7092cf76885261992d3fd829c68b94cbec9b7e
Allen Rohner <arohner@gmail.com>
2014-04-22 14:42:53 -0700

    re-add core.typed as a dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject circleci/schema-typer "0.2.1"
-  :dependencies [[prismatic/schema "0.2.1"]]
+  :dependencies [[org.clojure/core.typed "0.2.31"]
+                 [prismatic/schema "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
