beceff3aa5eab6c885a0f0d340e0f642741899e0
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-07-04 20:43:19 +0800

    Prepare for 0.2.3 release under com.ambrosebs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circleci/schema-typer "0.2.2"
+(defproject com.ambrosebs/schema-typer "0.2.3"
   :dependencies [[org.clojure/core.typed "0.2.53"]
                  [prismatic/schema "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
